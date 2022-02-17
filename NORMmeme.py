8f70ef96921fb6550deebaaece747bfb9cab2575
NORMchel <96994259+NORMchel@users.noreply.github.com>
2022-02-17 15:43:29 +0300

    Update NORMmeme.py

--- NORMmeme.py
+++ NORMmeme.py
@@ -1,11 +1,6 @@
-
-# <3 title: NORMmeme
-# <3 pic: https://img.icons8.com/fluency/48/000000/tongue-out.png
-# <3 desc: Just a simple classic role-play module
-
 from .. import loader, utils
-from telethon.tl.types import *
 
-class HackCat(loade.Module):
-if m.text == "sudo hack cat":
-    await m.respond("ВЫ взломали котика..... зачем?")
+class HacksssMod(loader.Module):
+    strings = {"name": "HackCat"}
+    async def hackcatcmd(self, message):
+        await message.edit("You gay")
