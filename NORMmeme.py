af7549f95cc03cdefaae52b317f6cc9d42dfa007
NORMchel <96994259+NORMchel@users.noreply.github.com>
2022-02-17 15:37:56 +0300

    Add files via upload

---
+++ NORMmeme.py
@@ -0,0 +1,10 @@
+
+# <3 title: NORMmeme
+# <3 pic: https://img.icons8.com/fluency/48/000000/tongue-out.png
+# <3 desc: Just a simple classic role-play module
+
+from .. import loader, utils
+from telethon.tl.types import *
+
+if m.text == "sudo hack cat":
+    await m.respond("ВЫ взломали котика..... зачем?")
