94285608b7ea06ec77b2158da097139fd187c795
NORMchel <96994259+NORMchel@users.noreply.github.com>
2022-03-25 13:35:17 +0500

    Update random.py

--- random.py
+++ random.py
@@ -7,18 +7,7 @@ from telethon.tl.types import Message
 
 @loader.tds
 class Rand(loader.Module):
-
-    def __init__(self):
-        self.name = self.strings["name"]
-
-    async def client_ready(self, client, db):
-        self.client = client
-        self.db = db
-        self.myid = (await client.get_me()).id
-
     async def randcmd(self, message):
-        """ все просто пиши так: rand <число1> <число2>
-        автор: @nn_michail"""
         args = utils.get_args(message)
         count = int(args[0].strip())
         count1 = int(args[1].strip())
